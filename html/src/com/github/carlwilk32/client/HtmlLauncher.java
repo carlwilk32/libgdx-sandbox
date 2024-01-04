d9532c3dfb101b047cd244abff4497e9b6f1561b
Igor Gulyayev <igor@donutsocial.com>
2024-01-04 14:21:37 -0800

    feat: simple game

--- html/src/com/github/carlwilk32/client/HtmlLauncher.java
+++ html/src/com/github/carlwilk32/client/HtmlLauncher.java
@@ -7,16 +7,16 @@ import com.github.carlwilk32.Main;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+//                return new GwtApplicationConfiguration(true);
+        // Fixed size application:
+        return new GwtApplicationConfiguration(800, 480);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new Main();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new Main();
+    }
 }
